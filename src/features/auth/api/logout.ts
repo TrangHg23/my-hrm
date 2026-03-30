fda827f88264106d636f1a6978bd948e5d33cc87
Hoang Trang <trang010280@gmail.com>
2026-03-30 15:58:15 +0700

    refactor: migrate to cookie-based authentication

--- src/features/auth/api/logout.ts
+++ src/features/auth/api/logout.ts
@@ -1,7 +1,7 @@
 import { api } from "@/lib/axios";
 
-export const logoutApi = async (token: string) => {
-  const res = await api.post("/auth/logout", { token });
+export const logoutApi = async () => {
+  const res = await api.post("/auth/logout");
 
   return res.data;
 };
